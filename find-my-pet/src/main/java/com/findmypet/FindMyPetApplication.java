a5954349f68ab275d85cb0db4e0a5a985c805ff3
osipover <osipov.sasha03@gmail.com>
2023-07-04 23:02:39 +0700

    Project directrory was created

---
+++ find-my-pet/src/main/java/com/findmypet/FindMyPetApplication.java
@@ -0,0 +1,13 @@
+package com.findmypet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMyPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMyPetApplication.class, args);
+	}
+
+}
