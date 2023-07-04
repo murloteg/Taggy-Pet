055a06e72b8753e2ba73fef2c449248e9b958c9b
osipover <osipov.sasha03@gmail.com>
2023-07-04 23:49:11 +0700

    New directory

---
+++ find-my-pet/src/test/java/com/findmypet/FindMyPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findmypet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMyPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
