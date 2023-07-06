2ce1603c184a764558dc5bed4a044320b4334929
Kirill Bolotov <k.y.bolotov@gmail.com>
2023-07-06 12:28:01 +0700

    Added variables to application.properties

--- find-my-pet/src/main/java/ru/nsu/sberlab/application/FindMyPetApplication.java
+++ find-my-pet/src/main/java/ru/nsu/sberlab/application/FindMyPetApplication.java
@@ -1,4 +1,4 @@
-package ru.nsu.sberlab;
+package ru.nsu.sberlab.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
